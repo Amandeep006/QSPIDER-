edc8eb5cd7593087c3c8b7bfbff3d066ddd3eae1
Amandeep <184535622+Amandeep006@users.noreply.github.com>
2026-07-22 21:33:31 +0530

    assignment-9

---
+++ Python/n1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+np.__version__
+
+# n=np.array([1,2,5,7],[5,8,6,3])
+# print(n) 
+
+
+# It will generate in number random values.
+# va=np. random.rand(5,10)
+# print(va)
+
+nr=np.random.randn(6)
+print(nr)
+
+
--- Python/numpy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# To create an array
-# var=np.array(collection)
-
-i=[1,2,3,4,5,6]
-
-var=np.array(i)
-print(var)
