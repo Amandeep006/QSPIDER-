d5bf6e245f1651a009f1dd19cc84341f5a0f8f1b
Amandeep <184535622+Amandeep006@users.noreply.github.com>
2026-07-27 15:38:05 +0530

    basic

--- Python/basic.py
+++ Python/basic.py
@@ -37,4 +37,68 @@
 # print(f"Simple interest of the user is {(principle*rate*time)/100}")
 
 
+"""WAPT CONVERT CELCIUS INTO FREHANITE. """
 
+# n=float("Enter the temperature in celcius : ")
+
+
+"""WAPT SWAP THE VALUE OF A VARIABLE USING TWO VARIABLE"""
+# a=int(input("Enter the X value :"))
+# b=int(input("Enter the Y value :"))
+# print(f"Before Swapping : a ={a} and b ={b}.")
+# a,b=b,a
+# print(f"After Swapping : a ={a} and b ={b}.")
+
+# without multiplt assigning 
+# a=a+b
+# b=a-b
+# a=a-b
+
+# a=a*b
+# b=a/b
+# a=a/b
+
+
+
+# print(f"After Swapping : a ={a} and b ={b}.")
+
+
+"""WAPT SWAP THE VALUES OF A VARIABLE USING THREE VARIABLE"""
+# a=int(input("Enter the X value :"))
+# b=int(input("Enter the Y value :"))
+# print(f"Before Swapping : a = {a} and b = {b}.")
+# temp=a
+# a=b
+# b=temp
+# print(f"After Swapping : a = {a} and b = {b}.")
+
+
+"""WAPT CONVERT DECIMAL INTO BINARY, OCTOR AND HEXADECIMAL NUMBER"""
+# num=int(input("Enter the number : "))
+# print(f"Binay number : {bin(num)[2:]}")
+# print(f"Hexdecimal number : {hex(num)[2:]}")
+# print(f"Octant number : {oct(num)[2:]}")
+
+""" WAPT CHECK WHETHER A GIVEN NUMBER IS EVEN OR NOT , IF IT IS EVEN THEN SQUARE THE NUMBER. """
+# num=int(input("Enter the number : "))
+# if num%2==0:
+#     print(f"Square of number : {num**2}")
+
+# else:
+#     print("It is not a even number")
+
+
+"""WAPT CHECK WHETHER A GIVEN NUMBER IS ODD OR NOT, IF IT IS ODD THEN CUBE THE NUMBER. """
+# num=int(input("Enter the number :"))
+# if num%2!=0:
+#     print(f"cubic of number : {num**3}")
+
+""" WAPT CHECK WHETHER A GIVEN CHARACTER IS ALPABHENT OR NOT , IF IT IS ALPHABET THEN PRINT HELLO WOROLD """
+
+str=input("Enter any world : ")
+# if str.isalpha():
+if "A"<=str<="Z" or "a"<=str<="z":
+    print("Hello World ")
+
+# wapt a given check is upper case or not, if it is upper case then print that charcter.
+# wapt to check whether a given number is divisible by both 3 and 7 or not, if it is divisible by both then print number is divisible by both.
--- Python/n1.py
+++ Python/n1.py
@@ -1,16 +1,20 @@
 import numpy as np
 
 """Array initialisation """
-# 1D array 
-a=np.array([1,2,3])
-print(a)
+# # 1D array 
+# a=np.array([1,2,3])
+# print(a)
 
-# 2D array 
-b=np.array([(15,2,3,5),(4,5,6,8)])
-print(b)
+# # 2D array 
+# b=np.array([(15,2,3,5),(4,5,6,8)])
+# print(b)
 
-# 3D array 
-c=np.array([(1,2,3,4),(5,6,7,8),(9,1,2,3)])
-print(c)
+# # 3D array 
+# c=np.array([(1,2,3,4),(5,6,7,8),(9,1,2,3)])
+# print(c)
 
 
+x=[1,2,3]
+y=x
+x=[4,5,6]
+print(y)
