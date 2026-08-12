905046bdc841662654edd48d876271dd1a83cb18
Amandeep <184535622+Amandeep006@users.noreply.github.com>
2026-08-12 22:59:47 +0530

    important1

---
+++ Python/important_interview_question.py
@@ -0,0 +1,73 @@
+"""IMPORTANT INTERVIEW QUESTIONS """
+
+
+"""
+WAPT SOLVE THE GIVEN BELOW REQUIREMENT.
+INPUT--> WE ARE LEARNING PYTHON AND IT IS EASY TO LEARN.
+OUTPUT--> LEARN TO EASY IS IT AND PYTHON LEARNING ARE WE.
+"""
+
+# s=input("Enter your sting : ")
+# l=s.split()
+# value=l[::-1]
+# # USE JOIN TO  {  " ".JOIN(VALUE)      }     
+# out=(" ".join(value))
+# print(out)
+
+
+"""
+WAPT SOLVE THE GIVEN BELOW REQUIREMENT.
+INPUT-->  WE LOVE TO CODE  IN PYTHON.
+OUTPUT-->  WE EVOL TO EDOC IN NOHTYP.
+"""
+
+"""use of for loop"""
+# s=input("Enter the string : ")
+# l=s.split()
+# for i in range(0,len(l)-1):
+#     if i%2!=0:
+#         l[i]=l[i][::-1]
+
+# out=(" ".join(l))
+# print(out)
+
+""" while loop """
+# s=input("Enter the string : ")
+# l=s.split()
+# a=[]
+# i=0
+# while i<len(l):
+#     if i%2==0:
+#         a.append(l[i])
+#     else:
+#         l[i]=l[i][::-1]
+#         a.append(l[i])
+
+#     i+=1
+
+# out=(" ".join(l))
+# print(out)
+
+
+"""WAPT SOLVE THE GIVEN BELOW REQUIREMENT. 
+INPUT--> WE LOVE TO CODE IN PYTHON 
+OUTPUT--> NOHTYP NI EDOC OT EVOL EW.
+"""
+
+""" Using only string concept """
+# s=input("Enter the string : ")
+# print(s[::-1])
+
+"""Using while loop"""
+# s=input("Enter the string : ")
+# l=s.split()
+# l1=l[::-1]
+# res=[]
+# for i in l1:
+#     res.append(i[::-1])
+
+# out=" ".join(res)
+# print(out)
+
+
+
