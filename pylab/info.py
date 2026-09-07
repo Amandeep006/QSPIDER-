fe0361ffd29ea67f70d0169604ab2a1b7573da33
Amandeep <184535622+Amandeep006@users.noreply.github.com>
2026-09-08 00:27:47 +0530

    new file

---
+++ pylab/info.py
@@ -0,0 +1,17 @@
+""""
+DATA SCIENCE : It is the process of extracting the past data to train a model. So that model can predict the future. 
+
+DIFFERENCE B/T DATABASE AND DATAWHAREHOUSE 
+1.
+database : It is used to store less amount of data.
+datawharehouse : It is used to store large amount of data.
+2. 
+database : In stores the data in structured way(in rows and columns).
+datawharehouse : In storse the data in unstructured ways (audio, video, docs,pdf,etc).
+3. 
+database : mysql, sql, postgressql etc.
+datawharehouse : cloud, google-cloud, microsoft-azure etc.
+
+
+
+"""
